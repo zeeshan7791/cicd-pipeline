c4df8b8f8c5d30f76154661336051df95267cd20
Zeeshan <zeeshanento34@gmail.com>
2025-07-26 12:31:49 +0500

    test

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 
 // takes params from url and returns a response
 app.get("/:id", (req, res) => {
-  res.send(`Hello hitesh World ${req.params.id}`);
+  res.send(`Hello World ${req.params.id}`);
 });
 
 // Only listen if not in test
